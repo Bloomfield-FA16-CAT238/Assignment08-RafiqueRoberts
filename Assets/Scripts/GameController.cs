066c864b90a213bd10e076899cbca86b18d981a5
Rafique Roberts <rafique_roberts@bloomfield.edu>
2016-11-08 16:09:32 -0500

    Finished assignment 8

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	Text hudText;
+
+	public int score;
+	public int lives;
+	public float timeInGame;
+	public int currentLevel;
+    public int teleport;
+
+
+
+    void Awake() {
+		if (PlayerPrefs.HasKey ("score")) {
+			RestorePlayerValues ();
+		} else {
+			StartNewGame ();
+		}
+	}
+
+	void Start () {
+      
+
+        // We need to call this in start so that when this is called on other levels
+        // we have access to the hudText and not 
+        hudText = GameObject.Find("HUDText").GetComponent<Text>();
+	}
+	
+	void Update () {
+		string hudInfo = "";
+
+		timeInGame += Time.deltaTime;
+
+		hudInfo += "Level: " + (currentLevel+1) + "\n";
+		hudInfo += "Score: " + score + "\n";
+		hudInfo += "Lives: " + lives + "\n";
+		hudInfo += "Time: " + timeInGame.ToString ("F2") + "\n";
+        hudInfo += "Teleports: " + teleport + "\n";
+
+		hudText.text = hudInfo;
+	}
+
+	public void StartNewGame() {
+		if (PlayerPrefs.HasKey ("score")) {
+			DeletePlayerValues();
+		}
+		score = 0;
+		timeInGame = 0;
+		lives = 3;
+		currentLevel = 0;
+        teleport = 0;
+		StorePlayerValues ();
+	}
+
+	public void LevelUp() {		
+		currentLevel = SceneManager.GetActiveScene().buildIndex + 1;
+		LoadLevel();
+        teleport += 1;
+	}
+
+	public void LevelDown() {
+		currentLevel = SceneManager.GetActiveScene().buildIndex - 1;
+		LoadLevel();
+        teleport += 1;
+    }
+
+	private void LoadLevel() {
+		StorePlayerValues ();
+		SceneManager.LoadScene(currentLevel);
+	}
+
+	public void CollectCoin() {
+		score += 100;
+	}
+
+	void StorePlayerValues() {
+		PlayerPrefs.SetInt ("score", score);
+		PlayerPrefs.SetInt ("lives", lives);
+		PlayerPrefs.SetFloat ("timeInGame", timeInGame);
+		PlayerPrefs.SetInt ("currentLevel", currentLevel);
+        PlayerPrefs.SetInt("teleport", teleport);
+    }
+
+	void RestorePlayerValues() {
+		score = PlayerPrefs.GetInt ("score");
+		lives = PlayerPrefs.GetInt ("lives");
+		timeInGame = PlayerPrefs.GetFloat ("timeInGame");
+		currentLevel = PlayerPrefs.GetInt ("currentLevel");
+        teleport = PlayerPrefs.GetInt("teleport");
+    }
+
+	void DeletePlayerValues() {
+		PlayerPrefs.DeleteKey ("score");
+		PlayerPrefs.DeleteKey ("lives");
+		PlayerPrefs.DeleteKey ("timeInGame");
+		PlayerPrefs.DeleteKey ("currentLevel");
+        PlayerPrefs.DeleteKey("teleport");
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public Slider healthBar;
+    public int health;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = 100;
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+
+        {
+            health -= 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (healthBar.name == "HealthBar")
+        {
+            healthBar.value = health;
+        }
+
+    }
+}
